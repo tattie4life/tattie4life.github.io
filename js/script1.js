75207b2229bad89b73dd31f2a7c2a8ea91bee86a
Jason <jason_tait@sky.com>
2020-04-06 19:28:25 +0100

    after javascript points system

--- js/script1.js
+++ js/script1.js
@@ -2,8 +2,7 @@ var modalBtns = document.querySelectorAll(".modal-open");
 
 modalBtns.forEach(function(btn){
 	btn.onclick= function() {
-		var modal = btn.getAttribute("data-modal");
-		
+		var modal = btn.getAttribute("data-modal");	
 		document.getElementById(modal).style.display = "block";	
 		
 	};
@@ -16,16 +15,44 @@ closeBtns.forEach(function(btn){
 	btn.onclick = function(){
 		var modal = (btn.closest(".modal").style.display = "none");
 	}
-})
+});
+
+
+var current_score = localStorage.getItem("current_score");
+document.getElementById("score").innerHTML = current_score;
+
+function quad_coin(){
+	var currentVal = document.getElementById("score").innerHTML;
+	var newVal = parseInt(currentVal) + 4;
+	localStorage.setItem("current_score", newVal);
+	document.getElementById("score").innerHTML = newVal;
+}
+
+function treble_coin(){
+	var currentVal = document.getElementById("score").innerHTML;
+	var newVal = parseInt(currentVal) + 3;
+	localStorage.setItem("current_score", newVal);
+	document.getElementById("score").innerHTML = newVal;
+}
+
+function double_coin(){
+	var currentVal = document.getElementById("score").innerHTML;
+	var newVal = parseInt(currentVal) + 2;
+	localStorage.setItem("current_score", newVal);
+	document.getElementById("score").innerHTML = newVal;
+}
+
+function one_coin(){
+	var currentVal = document.getElementById("score").innerHTML;
+	var newVal = parseInt(currentVal) + 1;
+	localStorage.setItem("current_score", newVal);
+	document.getElementById("score").innerHTML = newVal;
+}
 
 
 
-//var challenge_points = 2;
 
-//function score_2() {
-//var parseInt()
-//var new_total = c_score + challenge_points;
-//localStorage.setItem("current_score", new_total);}
+
 
 
 
